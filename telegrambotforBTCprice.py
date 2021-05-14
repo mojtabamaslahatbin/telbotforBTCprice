5bdc43c5f1d877e65748525d4bee3201f137f376
Mojtaba Maslahatbin <mmaslahatbin@gmail.com>
2021-05-14 16:26:46 +0430

    update code

--- telegrambotforBTCprice.py
+++ telegrambotforBTCprice.py
@@ -1,28 +1,32 @@
 from telegram.ext import Updater, CommandHandler
+from telegram.ext.dispatcher import run_async
 import requests
 
-
 def get_price():
-    contents = requests.get(
-        'https://api.coinbase.com/v2/prices/spot?currency=USD')
-    price = contents.json()['data']['amount']
+    contents = requests.get('https://api.coinbase.com/v2/prices/spot?currency=USD')
+    body = contents.json()
+    data = body.get('data', {})
+    price = data.get('amount', {})
+    if not price:
+        raise Exception("Price data was empty")
     return price
 
-
-def bop(bot, update):
+@run_async
+def btc(update, context):
     price = get_price()
     chat_id = update.message.chat_id
-    bot.send_message(
-        chat_id=chat_id, text="at this time BTC price is :" + price)
-
+    context.bot.send_message(chat_id=chat_id, text= f"BTC price is : {price}")
 
 def main():
-    updater = Updater('your telegram bot token')
+    updater = Updater('TELEGRAM BOT TOKEN')
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop', bop))
+    dp.add_handler(CommandHandler('btc', btc))
     updater.start_polling()
     updater.idle()
 
 
 if __name__ == '__main__':
     main()
+
+
+
